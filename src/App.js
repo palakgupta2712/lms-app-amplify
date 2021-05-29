ef0e35be642450bf38869a5f1f0b8d085dd6f760
Palak Gupta <palakgupta2712@gmail.com>
2021-05-29 16:31:48 +0530

    Material UI Custom Theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { BrowserRouter as Router } from "react-router-dom";
+import theme from "./theme";
 import Routes from "./routes/Routes";
-
 export default function App() {
   return (
     <React.Fragment>
+      <CssBaseline />
       <Router>
-        <Routes />
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
       </Router>
     </React.Fragment>
   );
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#36122C",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#BB7B3E",
+      contrastText: "#000",
+    },
+  },
+});
+
+export default theme;
