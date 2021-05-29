c4e6797b41fd09fff8ab613f06ac8b020d75857b
Palak Gupta <palakgupta2712@gmail.com>
2021-05-29 16:28:07 +0530

    SignIn & SignUp routes and components created

---
+++ src/auth/SignIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignIn() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+}
+
+export default SignIn;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import SignIn from "../auth/SignIn";
+import SignUp from "../auth/SignUp";
 import Home from "../components/Home";
 import LandingPage from "../components/LandingPage";
 
@@ -7,7 +9,9 @@ export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={LandingPage} />
-      <Route exact path="/home" component={Home} />{" "}
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/signin/" component={SignIn} />
+      <Route exact path="/signup/" component={SignUp} />
     </Switch>
   );
 }
