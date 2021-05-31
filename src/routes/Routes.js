54ea6440ab08d64a58e4ee7c27444af591b15ba9
Palak Gupta <palakgupta2712@gmail.com>
2021-05-31 14:04:06 +0530

    components: Error page added

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import SignIn from "../auth/SignIn";
 import SignUp from "../auth/SignUp";
+import Error404 from "../components/Error404";
 import Home from "../components/Home";
 import LandingPage from "../components/LandingPage";
 import Topic from "../components/Topic";
+import Courses from "../course/Courses";
+import NewCourse from "../course/NewCourse";
 import PrivateRoute from "./PrivateRoute";
 
 export default function Routes() {
@@ -15,6 +18,8 @@ export default function Routes() {
       <Route path="/signin/" component={SignIn} />
       <Route path="/signup/" component={SignUp} />
       <PrivateRoute path="/topic/" component={Topic} />
+
+      <Route path="*" component={Error404} />
     </Switch>
   );
 }
