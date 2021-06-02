0906c5a820d8337ff9ecbcd5da4d395e348b6585
Palak Gupta <palakgupta2712@gmail.com>
2021-06-02 18:47:45 +0530

    ProtectedRoutes for course component added

--- src/course/NavRoutes.js
+++ src/course/NavRoutes.js
@@ -1,31 +1,35 @@
 import React from "react";
-import { Route, Switch, useRouteMatch } from "react-router";
-import Announcements from "./pages/Announcements";
-import Assignments from "./pages/Assignments";
-import Chat from "./pages/Chat";
-import Discussions from "./pages/Discussions";
-import Files from "./pages/Files";
-import Home from "./pages/Home";
-import Lessons from "./pages/Lessons";
-import Notes from "./pages/Notes";
-import Syllabus from "./pages/Syllabus";
-import Zoom from "./pages/Zoom";
+import { Switch, useRouteMatch } from "react-router";
+import ProtectedRoutes from "./ProtectedRoutes";
+import Announcements from "../pages/Announcements";
+import Assignments from "../pages/Assignments";
+import Chat from "../pages/Chat";
+import Discussions from "../pages/Discussions";
+import Files from "../pages/Files";
+import Home from "../pages/Home";
+import Lessons from "../pages/Lessons";
+import Notes from "../pages/Notes";
+import Syllabus from "../pages/Syllabus";
+import Zoom from "../pages/Zoom";
 function NavRoutes() {
   let { path } = useRouteMatch();
 
   return (
     <div style={{ width: "100%" }}>
       <Switch>
-        <Route path={`${path}/home`} component={Home} />
-        <Route path={`${path}/announcements`} component={Announcements} />
-        <Route path={`${path}/assignments`} component={Assignments} />
-        <Route path={`${path}/lessons`} component={Lessons} />
-        <Route path={`${path}/discussions`} component={Discussions} />
-        <Route path={`${path}/syllabus`} component={Syllabus} />
-        <Route path={`${path}/files`} component={Files} />
-        <Route path={`${path}/notes`} component={Notes} />
-        <Route path={`${path}/zoom`} component={Zoom} />
-        <Route path={`${path}/chat`} component={Chat} />
+        <ProtectedRoutes path={`${path}/home`} component={Home} />
+        <ProtectedRoutes
+          path={`${path}/announcements`}
+          component={Announcements}
+        />
+        <ProtectedRoutes path={`${path}/assignments`} component={Assignments} />
+        <ProtectedRoutes path={`${path}/lessons`} component={Lessons} />
+        <ProtectedRoutes path={`${path}/discussions`} component={Discussions} />
+        <ProtectedRoutes path={`${path}/syllabus`} component={Syllabus} />
+        <ProtectedRoutes path={`${path}/files`} component={Files} />
+        <ProtectedRoutes path={`${path}/notes`} component={Notes} />
+        <ProtectedRoutes path={`${path}/zoom`} component={Zoom} />
+        <ProtectedRoutes path={`${path}/chat`} component={Chat} />
       </Switch>
     </div>
   );
