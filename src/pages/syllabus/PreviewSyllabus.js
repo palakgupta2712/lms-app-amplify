d987e7f5b2043c34650e8be58fb8509e7dc8d7d0
Palak Gupta <palakgupta2712@gmail.com>
2021-06-13 02:22:14 +0530

    new pages added (#30)
    - Syllabus page added
    - Home page added
    - Error page updated

---
+++ src/pages/syllabus/PreviewSyllabus.js
@@ -0,0 +1,66 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Button } from "@material-ui/core";
+import { Storage } from "aws-amplify";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import { Container } from "@material-ui/core";
+import { UserContext } from "../../context/UserContext";
+
+const options = {
+  cMapUrl: "cmaps/",
+  cMapPacked: true,
+};
+
+function PreviewSyllabus({ syllabus, handleDelete }) {
+  const user = useContext(UserContext);
+  const [url, setUrl] = useState();
+  const [numPages, setNumPages] = useState(null);
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
+  useEffect(() => {
+    loadFileUrl(syllabus);
+  }, []);
+
+  const loadFileUrl = async (syllabus) => {
+    await Storage.get(syllabus.S3Key).then((res) => {
+      setUrl(res);
+      console.log(res);
+    });
+  };
+  return (
+    <>
+      {user.username === syllabus.uploadedBy && (
+        <Button
+          onClick={() => {
+            handleDelete(syllabus.id);
+          }}
+          variant="contained"
+          color="primary"
+          style={{ float: "right" }}
+        >
+          Delete
+        </Button>
+      )}
+      <Container className="Example__container__document" maxWidth="lg">
+        <br />
+        <Document
+          file={url}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+        >
+          {Array.from(new Array(numPages), (el, index) => (
+            <div>
+              {" "}
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+              <p style={{ display: "flex", justifyContent: "center" }}>
+                Page {index + 1} of {numPages}
+              </p>
+            </div>
+          ))}
+        </Document>
+      </Container>
+    </>
+  );
+}
+
+export default PreviewSyllabus;
